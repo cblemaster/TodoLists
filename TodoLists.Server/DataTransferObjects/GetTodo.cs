1f412ce3c093c04ccef5fe6bd1d0b50220d3e7e5
Brian LeMaster <brian.lemaster@outlook.com>
2024-09-04 11:36:52 -0400

    fixed todo dtos in backend project - needed due to todo entity changes

--- TodoLists.Server/DataTransferObjects/GetTodo.cs
+++ TodoLists.Server/DataTransferObjects/GetTodo.cs
@@ -1,4 +1,4 @@
 ﻿namespace TodoLists.Server.DataTransferObjects;
 
-public record GetTodo(int TodoId, int TodoListId, string Description, DateOnly?
+public record GetTodo(int TodoId, int TodoListId, string Description, DateTime?
     DueDate, bool IsImportant, bool IsComplete);
