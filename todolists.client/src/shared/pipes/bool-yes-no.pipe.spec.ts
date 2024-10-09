4189dd5782238e06660275a641a3a1f60c263314
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-08 21:03:15 -0400

    deleted test files from client project (yagni)

--- todolists.client/src/shared/pipes/bool-yes-no.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BoolYesNoPipe } from './bool-yes-no.pipe';
-
-describe('BoolYesNoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BoolYesNoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
