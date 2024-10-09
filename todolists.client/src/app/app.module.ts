8d297b5d3ba7dc15dc370418373600e660ba4dd0
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-08 21:05:02 -0400

    removed refs to deleted components from app module in client project

--- todolists.client/src/app/app.module.ts
+++ todolists.client/src/app/app.module.ts
@@ -4,25 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
-import { CreatetodolistComponent } from './createtodolist/createtodolist.component';
-import { RenametodolistComponent } from './renametodolist/renametodolist.component';
-import { DeletetodolistComponent } from './deletetodolist/deletetodolist.component';
-import { CreatetodoComponent } from './createtodo/createtodo.component';
-import { UpdatetodoComponent } from './updatetodo/updatetodo.component';
-import { TodoComponent } from './todo/todo.component';
-import { DeletetodoComponent } from './deletetodo/deletetodo.component';
 import { BoolYesNoPipe } from '../shared/pipes/bool-yes-no.pipe';
 
 @NgModule({ declarations: [
         AppComponent,
         TodolistComponent,
-        CreatetodolistComponent,
-        RenametodolistComponent,
-        DeletetodolistComponent,
-        CreatetodoComponent,
-        UpdatetodoComponent,
-        TodoComponent,
-        DeletetodoComponent,
         BoolYesNoPipe,
     ],
     bootstrap: [AppComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
