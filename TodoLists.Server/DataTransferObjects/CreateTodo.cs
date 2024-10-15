43d3779bbc10c0e41a1ceee99be225a07ae92566
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-14 21:19:44 -0400

    added dtos to server project, copied from another project

---
+++ TodoLists.Server/DataTransferObjects/CreateTodo.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace TodoLists.Server.DataTransferObjects;
+
+public class CreateTodo
+{
+    public int TodoListId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateOnly? DueDate { get; set; }
+    public bool IsImportant { get; set; }
+    public bool IsComplete { get; set; }
+    public (bool IsValid, string ErrorMessage) Validate() {
+        bool isValid = false;
+        StringBuilder errorMessage = new();
+
+        if (TodoListId < 1) { errorMessage.Append("Invalid todo list id"); }
+        if (string.IsNullOrWhiteSpace(Description)) { errorMessage.Append("Description is required"); }
+        if (Description.Length < 1 || Description.Length > 255) { errorMessage.Append("Description is required and must be 255 characters or fewer"); }
+        else { isValid = true; }
+
+        return (isValid, errorMessage.ToString());
+    }
+}
---
+++ TodoLists.Server/DataTransferObjects/CreateTodoList.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace TodoLists.Server.DataTransferObjects;
+
+public class CreateTodoList
+{
+    public string Name { get; set; } = string.Empty;
+
+    public (bool IsValid, string ErrorMessage) Validate() {
+        bool isValid = false;
+        StringBuilder errorMessage = new();
+
+        if (string.IsNullOrWhiteSpace(Name)) { errorMessage.Append("Name is required"); }
+        if (Name.Length < 1 || Name.Length > 255) { errorMessage.Append("Name is required and must be 255 characters or fewer"); }
+        else { isValid = true; }
+
+        return (isValid, errorMessage.ToString());
+    }
+}
