6960dea3ca7f3194f4244c250087e8c579b7f45e
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-14 20:40:58 -0400

    added scaffolded db content and entity classes to server project

---
+++ TodoLists.Server/TodoList.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoLists.Server;
+
+public partial class TodoList
+{
+    public int TodoListId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public virtual ICollection<Todo> Todos { get; set; } = [];
+}
