ef2be16f543bf37cd530d43f4dae081119a565aa
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-16 18:00:02 -0400

    updated shared module in client project

--- todolists.client/src/shared/shared.module.ts
+++ todolists.client/src/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoolYesNoPipe } from './pipes/bool-yes-no.pipe';
+import { Todo } from './models/todo';
+import { TodoList } from './models/todo-list';
 
-@NgModule({ declarations: [ BoolYesNoPipe ], imports: [ CommonModule ] })
+@NgModule({ declarations: [BoolYesNoPipe, Todo, TodoList], imports: [CommonModule] })
 
 export class SharedModule { }
