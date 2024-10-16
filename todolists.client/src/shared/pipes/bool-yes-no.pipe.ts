337774cf107164d9b3bd36e16ddaa57e42004f6e
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-16 16:53:28 -0400

    added shared module and bool to yes/no pipe in client project

---
+++ todolists.client/src/shared/pipes/bool-yes-no.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'boolYesNo' })
+
+export class BoolYesNoPipe implements PipeTransform {
+
+  transform(value: boolean): string {
+    return value ? 'Yes' : 'No';
+  }
+}
---
+++ todolists.client/src/shared/shared.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BoolYesNoPipe } from './pipes/bool-yes-no.pipe';
+
+@NgModule({ declarations: [ BoolYesNoPipe ], imports: [ CommonModule ] })
+
+export class SharedModule { }
