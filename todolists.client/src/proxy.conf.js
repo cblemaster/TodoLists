373a89161083ac483bbfaa3366a7baf71068f491
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-18 20:41:26 -0400

    updated proxy in client project for api routes

--- todolists.client/src/proxy.conf.js
+++ todolists.client/src/proxy.conf.js
@@ -6,7 +6,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/todolist",
+      "/todo"
     ],
     target,
     secure: false
