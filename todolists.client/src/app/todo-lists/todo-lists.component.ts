ec42ecdd86d2dd7f2a4747ac82070052f399ef66
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-18 20:44:13 -0400

    added todo lists component in client project

---
+++ todolists.client/src/app/todo-lists/todo-lists.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpDataService } from '../../http-data.service';
+import { TodoList } from '../../models/todo-list';
+
+@Component({
+  selector: 'app-todo-lists',
+  templateUrl: './todo-lists.component.html',
+  styleUrl: './todo-lists.component.css'
+})
+export class TodoListsComponent implements OnInit {
+  public todoLists : TodoList[] = [];
+  
+  constructor(private data: HttpDataService) { }
+  
+  ngOnInit() {
+    this.getTodoLists();
+  }
+
+  getTodoLists() {
+    this.data.listList().subscribe(
+      (result) => {
+        this.todoLists = result;
+      },
+      (error) => {
+        console.error(error)
+      }
+    );
+  }
+}
