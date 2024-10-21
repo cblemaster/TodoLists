a690d61ff44344b9e2ba3395da77a20d08841d18
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-21 08:14:59 -0400

    updated app component and module in client project, to accommodate removed components

--- todolists.client/src/app/app.component.ts
+++ todolists.client/src/app/app.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { TodoList } from '../models/todo-list';
+import { HttpDataService } from '../http-data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  title = 'Todo Lists';
-  
-  constructor() { }
+  // title = 'Todo Lists';
 
-  ngOnInit() {  }
+  todoLists : TodoList[] = [];
+  selectedTodoList : TodoList = new TodoList;
+  
+  constructor(private data: HttpDataService) { }
+  
+  ngOnInit() : void {
+    this.getTodoLists();
+  }
+
+  setSelectedTodoListId() {}
+
+  getTodoLists() : void {
+    this.data.listList().subscribe(
+      (result) => {
+        this.todoLists = result;
+      },
+      (error) => {
+        console.error(error)
+      }
+    );
+  }
+
+  createTodoList() {
+
+  }
+
+  /* todos : Todo[] | undefined;
+
+  constructor(private data: HttpDataService, private boolYesNo : BoolYesNoPipe) { }
+
+  ngOnInit(): void {
+    this.data.readList(6).subscribe(
+      (result) => {
+        this.todos = result.todos;
+      },
+      (error) => {
+        console.error(error)
+      }
+    );
+  }
+
+  selectedList: TodoList = new TodoList;  
+
+  constructor(private data: HttpDataService) { }
+  
+  ngOnInit(): void {
+    this.getSelectedList(5);
+  }
+
+  getSelectedList(id: number) : void {
+    this.data.readList(id).subscribe(
+      (result) => {
+        this.selectedList = result;
+      },
+      (error) => {
+        console.error(error)
+      }
+    );
+  } */
 }
--- todolists.client/src/app/app.module.ts
+++ todolists.client/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListsComponent } from './todo-lists/todo-lists.component';
 import { HttpDataService } from '../http-data.service';
 import { HttpClientModule } from '@angular/common/http';
-import { SelectedListComponent } from './selected-list/selected-list.component';
 
 @NgModule(
-  { declarations: [ AppComponent, TodoListsComponent, SelectedListComponent ],
+  { declarations: [ AppComponent ],
     imports: [ BrowserModule, AppRoutingModule, HttpClientModule ],
     providers: [ HttpDataService ],
     bootstrap: [AppComponent ] })
