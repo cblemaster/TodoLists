ccc1a802a1ffb94a42a0e957b2633b748d23bcda
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-21 12:28:43 -0400

    updated layout, styling, and behavior of app component in client project

--- todolists.client/src/app/app.component.ts
+++ todolists.client/src/app/app.component.ts
@@ -2,25 +2,25 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoList } from '../models/todo-list';
 import { HttpDataService } from '../http-data.service';
+import { Todo } from '../models/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent implements OnInit {
-  // title = 'Todo Lists';
 
   todoLists : TodoList[] = [];
-  selectedTodoList : TodoList = new TodoList;
+  selectedTodoList! : TodoList;
+  todos : Todo[] = [];
   
   constructor(private data: HttpDataService) { }
   
   ngOnInit() : void {
     this.getTodoLists();
   }
-
-  setSelectedTodoListId() {}
 
   getTodoLists() : void {
     this.data.listList().subscribe(
@@ -33,17 +33,10 @@ export class AppComponent implements OnInit {
     );
   }
 
-  createTodoList() {
-
-  }
-
-  /* todos : Todo[] | undefined;
-
-  constructor(private data: HttpDataService, private boolYesNo : BoolYesNoPipe) { }
-
-  ngOnInit(): void {
-    this.data.readList(6).subscribe(
+  setSelectedTodoList(id : number) : void {
+    this.data.readList(id).subscribe(
       (result) => {
+        this.selectedTodoList = result;
         this.todos = result.todos;
       },
       (error) => {
@@ -52,22 +45,8 @@ export class AppComponent implements OnInit {
     );
   }
 
-  selectedList: TodoList = new TodoList;  
-
-  constructor(private data: HttpDataService) { }
   
-  ngOnInit(): void {
-    this.getSelectedList(5);
-  }
+  createTodoList() {
 
-  getSelectedList(id: number) : void {
-    this.data.readList(id).subscribe(
-      (result) => {
-        this.selectedList = result;
-      },
-      (error) => {
-        console.error(error)
-      }
-    );
-  } */
+  }
 }
