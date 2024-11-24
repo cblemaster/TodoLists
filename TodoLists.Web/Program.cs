db24d9927db63417cef0c6278453903453511c3a
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-23 19:19:19 -0500

    hi level pseudocode for api endpoints in web project

--- TodoLists.Web/Program.cs
+++ TodoLists.Web/Program.cs
@@ -5,3 +5,26 @@ WebApplication app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+//  TODO >> bring validation & BR into this! It's a domain model!
+
+//  + get all todo lists, do not include todos
+//  + get todo list by id, including todos
+//	+ get all todos where DueDate == today && IsComplete == false;
+//	+ get all todos where IsImportant == true && IsComplete == false;
+//  + get all todos where IsComplete == true;
+
+//	+ (post) create todo list
+//	+ (post) create todo
+//  + (post) move todo to new list
+
+//	+ (put) rename todo list
+//	+ (put) mark complete/incomplete
+//	+ (put) change due date
+//	+ (put) toggle importance
+//	+ (put) change description
+//	+ (put) move to list
+//	+ (put) move to list
+
+//  + delete todo - only if IsImportant == false;
+//	+ delete todo list - only if ToDos.All(t => t.IsComplete)
