cb3894fa58aae7fcb10761f8f0bd7a6bd6b96170
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-28 08:47:44 -0500

    added infrastructure project with db context class

---
+++ TodoLists.Infrastructure/Data/TodoListDbContext.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TodoLists.Domain;
+
+namespace TodoLists.Infrastructure.Data;
+
+internal class TodoListDbContext : DbContext
+{
+    public DbSet<Todo> Todos { get; set; } = default!;
+    public DbSet<TodoList> TodoLists { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(@"Server=.;Database=ListDetails;Trusted_Connection=true;Trust Server Certificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Todo>(entity =>
+        {
+            entity.ToTable("Todo");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.HasOne(d => d.TodoList).WithMany(p => p.Todos)
+                .HasForeignKey(d => d.TodoListId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Todo_TodoList");
+        });
+
+        modelBuilder.Entity<TodoList>(entity =>
+        {
+            entity.ToTable("TodoList");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+    }
+}
