ec5f0a394bb09cb6e83ca2d1430ed2902fb91435
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-28 10:02:36 -0500

    added validation in api project, will need to split this out later so that other layers can use it

---
+++ TodoLists.API/Validation/StringValidationExtensions.cs
@@ -0,0 +1,42 @@
+﻿
+namespace TodoLists.API.Validation;
+
+public static class StringValidationExtensions
+{
+    // TODO >> this is the same logic as ValidateTodoDescription(), only the error messages differ
+    public static ValidationResult ValidateTodoListName(this string s)
+    {
+        bool isValid = true;
+        List<string> errors = [];
+
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            isValid = false;
+            errors.Add("List name is required and cannot be only whitespace characters.");
+        }
+        else if (s.Length > 255)
+        {
+            isValid = false;
+            errors.Add("List name cannot exceed 255 characters.");
+        }
+        return new ValidationResult() { IsValid = isValid, Errors = errors };
+    }
+
+    public static ValidationResult ValidateTodoDescription(this string s)
+    {
+        bool isValid = true;
+        List<string> errors = [];
+
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            isValid = false;
+            errors.Add("Todo description is required and cannot be only whitespace characters.");
+        }
+        else if (s.Length > 255)
+        {
+            isValid = false;
+            errors.Add("Todo description cannot exceed 255 characters.");
+        }
+        return new ValidationResult() { IsValid = isValid, Errors = errors };
+    }
+}
---
+++ TodoLists.API/Validation/ValidationResult.cs
@@ -0,0 +1,8 @@
+﻿
+namespace TodoLists.API.Validation;
+
+public sealed class ValidationResult
+{
+    public bool IsValid { get; init; }
+    public IEnumerable<string> Errors { get; init; } = [];
+}
