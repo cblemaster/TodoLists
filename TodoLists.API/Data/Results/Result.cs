aa9b79b35b7c45a6ac795960648292b501a74c69
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-28 13:44:15 -0500

    added results model to api project

---
+++ TodoLists.API/Data/Results/Result.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TodoLists.API.Data.Results;
+
+internal abstract class Result<T>
+{
+    internal required ResultType ResultType { get; init; }
+    internal required string Message { get; init; }
+    internal T Payload { get; init; } = default(T)!;
+}
---
+++ TodoLists.API/Data/Results/ResultType.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TodoLists.API.Data.Results;
+
+internal enum ResultType
+{
+    NotSet = 0,
+    Error = 1,
+    Invalid = 2,
+    NotFound = 3,
+    Success = 4,
+}
